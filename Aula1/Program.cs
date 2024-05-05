f99f0fd9f9fa39c48857d916112ac519d08d5c4c
Pedro L. <economy.pedrolucas@hotmail.com>
2024-05-04 21:00:18 -0400

    Aula1
    Just a "Hello World"

---
+++ Aula1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    //Lembrando que para rodar qualquer programa em C#, deve se utilizar o comando 
+    // dotnet new console
+    //E apos isso utilizar :
+    // dotnet run
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
